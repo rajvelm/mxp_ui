342113f66112b77f2f61ad2067c39cd5b35853e5
christ <christylavanya@gmail.com>
2018-05-11 10:39:50 +0530

    Grid with detail
    Grid with detail

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule}  from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {AgGridModule} from "ag-grid-angular";
 
 import { DragulaModule } from 'ng2-dragula/ng2-dragula';
 
 import { InventoryComponent } from './inventory.component';
+import { DetailPanelComponent } from './detail-panel.component';
+
 import { InventoryRoutes } from './inventory.routing';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(InventoryRoutes), DragulaModule],
-  declarations: [InventoryComponent]
+  imports: [CommonModule, RouterModule.forChild(InventoryRoutes), DragulaModule, FormsModule, HttpModule, AgGridModule.withComponents([DetailPanelComponent])],
+  declarations: [InventoryComponent,DetailPanelComponent],
+  bootstrap: [InventoryComponent]
 })
 
 export class InventoryModule {}
